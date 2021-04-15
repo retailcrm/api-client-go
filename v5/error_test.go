c8051587bf6f3841b8886187ca65b4c14e1608f3
Ruslan Efanov <rus.efanov@bk.ru>
2021-04-15 09:55:43 +0300

    change struct for errors

--- v5/error_test.go
+++ v5/error_test.go
@@ -1,50 +1,29 @@
 package v5
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestFailure_ApiErrorsSlice(t *testing.T) {
-	var err = Failure{}
 	b := []byte(`{"success": false, "errorMsg": "Failed to activate module", "errors": ["Your account has insufficient funds to activate integration module"]}`)
 	expected := map[string]string{
 		"0": "Your account has insufficient funds to activate integration module",
 	}
 
-	resp, e := ErrorResponse(b)
-	err.SetRuntimeError(e)
-	err.SetApiError(resp.ErrorMsg)
-
-	if resp.Errors != nil {
-		err.SetApiErrors(ErrorsHandler(resp.Errors))
-	}
-
-	eq := reflect.DeepEqual(expected, err.ApiErrors())
-
-	if eq != true {
+	e := NewApiError(b)
+	if eq := e.(*ApiError).Errors["0"] == expected["0"]; eq != true {
 		t.Errorf("%+v", eq)
 	}
 }
 
 func TestFailure_ApiErrorsMap(t *testing.T) {
-	var err = Failure{}
 	b := []byte(`{"success": false, "errorMsg": "Failed to activate module", "errors": {"id": "ID must be an integer"}}`)
 	expected := map[string]string{
 		"id": "ID must be an integer",
 	}
 
-	resp, e := ErrorResponse(b)
-	err.SetRuntimeError(e)
-	err.SetApiError(resp.ErrorMsg)
-
-	if resp.Errors != nil {
-		err.SetApiErrors(ErrorsHandler(resp.Errors))
-	}
-
-	eq := reflect.DeepEqual(expected, err.ApiErrors())
-
-	if eq != true {
+	e := NewApiError(b)
+	if eq := expected["id"] == e.(*ApiError).Errors["id"]; eq != true {
 		t.Errorf("%+v", eq)
 	}
 }
